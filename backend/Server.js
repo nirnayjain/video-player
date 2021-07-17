92a6d64c2c9bec6f3f6c900623a7cab8c15cdaee
Nirnay Jain <nirujainofficial@gmail.com>
2021-07-17 09:47:44 +0530

    cors changes

--- backend/Server.js
+++ backend/Server.js
@@ -32,7 +32,7 @@ app.use(cookieParser())
  app.use(express.json())
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:3000'
+    origin:'https://powerful-castle-62704.herokuapp.com'
 }))
 
 app.use('/api/user',route)
