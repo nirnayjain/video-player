29690ef5c7ecd94a0d85cb54bc6887bf84036b6d
Nirnay Jain <nirujainofficial@gmail.com>
2021-07-17 17:10:51 +0530

    bus

--- backend/Server.js
+++ backend/Server.js
@@ -40,9 +40,7 @@ app.use('/api/user',route)
 if(process.env.NODE_ENV==='production')
 {
     app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+   
 }
 
 const PORT = process.env.PORT || 4000
