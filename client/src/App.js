86d151aaa65ea1745c93960605f168929028fea9
Nirnay Jain <nirujainofficial@gmail.com>
2021-07-17 19:18:49 +0530

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Home from './Screens/Home'
 import Movies from './Screens/Movies'
+import Nav from './Components/Nav'
  
 function App() {
    return (
    <>
  
-     
+    <Nav /> 
       <Switch>
          <Route exact path="/" component={Home} />
           <Route path="/movies" component={Movies}/>
