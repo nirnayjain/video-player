ba90911e3313d622d7454db50954ba03d737c643
Nirnay Jain <nirujainofficial@gmail.com>
2021-11-29 11:51:18 +0530

    node env updated

--- backend/Server.js
+++ backend/Server.js
@@ -34,13 +34,13 @@ app.use(cookieParser())
 
 app.use('/api/user',route)
 const __dirname=path.resolve()
-if(process.env.NODE_ENV==='production')
+if(process.env.NODE_ENV!='development')
 {
     app.use(express.static('client/build'));
      app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
-   
+
 }
 else{
 
