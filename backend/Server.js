87d8141c77c4fe8902e0d93c3eefb19e6ffef46e
Nirnay Jain <nirujainofficial@gmail.com>
2021-12-02 11:26:35 +0530

    changes modified

--- backend/Server.js
+++ backend/Server.js
@@ -36,10 +36,10 @@ app.use('/api/user',route)
 const __dirname=path.resolve()
 if(process.env.NODE_ENV==='production')
 {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'build')));
      app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+        res.sendFile(path.join(__dirname,'build', 'index.html'));
+     })
 
 }
 else{
